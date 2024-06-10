d0dcab22f0a46a5674759fcb68dd7940c069275d
2022051615270夏瑞涛 <2312609759@qq.com>
2024-06-10 17:31:47 +0800

    version 0.0.1

--- widget.cpp
+++ widget.cpp
@@ -1,4 +1,6 @@
 #include "widget.h"
+#include <QMessageBox>
+#include <QRegularExpression>
 #include "./ui_widget.h"
 #include "centercontrol.h"
 Widget::Widget(CenterControl *cctrl, QWidget *parent)
@@ -32,6 +34,18 @@ void Widget::on_closeShareBtn_clicked()
 
 void Widget::on_linkBtn_clicked()
 {
+    QRegularExpression ipv4Regex("^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2["
+                                 "0-4][0-9]|[01]?[0-9][0-9]?)$");
+    QRegularExpression portRegex(
+        "^([0-9]{1,4}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$");
+
+    QRegularExpressionMatch ipv4Match = ipv4Regex.match(_ui->ipEdit->text());
+    QRegularExpressionMatch portMatch = portRegex.match(_ui->portEdit->text());
+
+    if (!ipv4Match.hasMatch() || !portMatch.hasMatch()) {
+        QMessageBox::warning(this, "", "ip or port error");
+        return;
+    }
     QString ip = _ui->ipEdit->text();
     unsigned short port = _ui->portEdit->text().toUShort();
     _cctrl->linkPc(ip, port);
